4594c6123f0946ec916ec9024cdb7e9470123cf8
Dustin Palmatier <dustinpalmatier@icloud.com>
2022-12-19 15:49:28 -0500

    Inproved Typing

--- src/utilities/responseBuilder.js
+++ src/utilities/responseBuilder.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {Response} res Server response
+ * @param {Number} status The status code returned
+ * @param {Boolean} error The error boolean.
+ * @param {*} data Data that will be passed;
+ * @returns {any}
+ */
 module.exports = (res, status = 200, error = false, data) => {
   return res.status(status).json({ error, data }).end();
 };
